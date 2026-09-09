c23111999c9ad120d7683491f2c7bede753a8fbd
JoaoGW <cpsenha@gmail.com>
2026-09-09 16:26:33 -0300

    (imp): Estrutura base da infraestrutura AKS

---
+++ environments/hml/main.tf
@@ -0,0 +1,55 @@
+data "terraform_remote_state" "foundation" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = var.state_resource_group_name
+    storage_account_name = var.state_storage_account_name
+    container_name       = var.aks_state_container_name
+    key                  = "foundation.tfstate"
+    use_azuread_auth     = true
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.foundation.outputs.aks_host
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.foundation.outputs.aks_cluster_ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "kubelogin"
+    args = [
+      "get-token",
+      "--login",
+      "azurecli",
+      "--server-id",
+      var.aks_server_application_id,
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.foundation.outputs.aks_host
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.foundation.outputs.aks_cluster_ca_certificate)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "kubelogin"
+      args = [
+        "get-token",
+        "--login",
+        "azurecli",
+        "--server-id",
+        var.aks_server_application_id,
+      ]
+    }
+  }
+}
+
+module "namespace" {
+  source                   = "../../modules/namespace"
+  namespace                = "hml"
+  create_shared_namespaces = true
+  install_kong             = true
+  kong_chart_version       = var.kong_chart_version
+}
---
+++ foundation/outputs.tf
@@ -0,0 +1,56 @@
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.shared.name
+}
+
+output "aks_host" {
+  value = azurerm_kubernetes_cluster.shared.kube_config[0].host
+}
+
+output "aks_cluster_ca_certificate" {
+  value     = azurerm_kubernetes_cluster.shared.kube_config[0].cluster_ca_certificate
+  sensitive = true
+}
+
+output "aks_oidc_issuer_url" {
+  value = azurerm_kubernetes_cluster.shared.oidc_issuer_url
+}
+
+output "platform_resource_group_name" {
+  value = data.azurerm_resource_group.platform.name
+}
+
+output "data_resource_group_name" {
+  value = data.azurerm_resource_group.data.name
+}
+
+output "postgresql_subnet_id" {
+  value = azurerm_subnet.postgresql.id
+}
+
+output "postgresql_private_dns_zone_id" {
+  value = azurerm_private_dns_zone.postgresql.id
+}
+
+output "key_vault_id" {
+  value = azurerm_key_vault.platform.id
+}
+
+output "key_vault_name" {
+  value = azurerm_key_vault.platform.name
+}
+
+output "key_vault_uri" {
+  value = azurerm_key_vault.platform.vault_uri
+}
+
+output "github_identity_client_ids" {
+  value = {
+    for key, identity in azurerm_user_assigned_identity.github : key => identity.client_id
+  }
+}
+
+output "api_workload_client_ids" {
+  value = {
+    for environment, identity in azurerm_user_assigned_identity.api_workload : environment => identity.client_id
+  }
+}
