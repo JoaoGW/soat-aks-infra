6a9e68c7e595621304a7bfffd021b099370a60b5
JoaoGW <cpsenha@gmail.com>
2026-09-11 13:01:15 -0300

    (fix): Limitado acesso kubectl à homologação

--- foundation/main.tf
+++ foundation/main.tf
@@ -323,7 +323,7 @@ resource "azurerm_role_assignment" "auth_key_vault" {
 resource "azurerm_role_assignment" "aks_cluster_admin" {
   for_each = {
     for key, identity in local.github_identities : key => identity
-    if identity.purpose == "deploy" && (startswith(key, "aks_") || startswith(key, "api_"))
+    if identity.purpose == "deploy" && (key == "aks_hml" || startswith(key, "api_"))
   }
 
   scope                = azurerm_kubernetes_cluster.shared.id
